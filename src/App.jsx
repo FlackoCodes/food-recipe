7730e80a7753d95f111567c48adefc524c3cecde
Flacko <lesliejoe2k2@gmail.com>
2024-07-28 22:11:25 +0000

    fixed tailwind config bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ function App() {
   return (
     <>
       <div>
-        <p className="bg-slate-500 text-xl">Trying tailwind</p>
+        <p className="bg-slate-500 text-xl mx-2">Trying tailwind</p>
+        <h2 className='text-center blur-md'>Food Recipe</h2>
+        <p className='bg-slate-900 text-red-700'>Tailwind now working</p>
       </div>
     </>
   );

d00f15f7050a101ae832530b0d9d05d45a2777fe
Flacko <lesliejoe2k2@gmail.com>
2024-07-31 07:13:21 +0000

    consumed API

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,24 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../context";
 
 export default function Navbar() {
+  const { searchParam, setSearchParam, handleSubmit } =
+    useContext(GlobalContext);
+  console.log(searchParam);
+
   return (
     <nav className="flex justify-between items-center py-8 container mx-auto  flex-col lg:flex-row gap-5 lg:gap-0">
       <NavLink to={"/"} className="text-2xl font-bold">
         FoodRecipe
       </NavLink>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <input
           type="text"
           name="search"
           placeholder="Enter Items...."
+          value={searchParam}
+          onChange={(e) => setSearchParam(e.target.value)}
           className="bg-white/75 p-3 px-8 outline-none rounded-full lg:w-96 shadow-lg shadow-gray-300 focus:shadow-gray-400"
         />
       </form>
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,9 +1,37 @@
-import {  createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
-export default function GlobalState({Children}){
-    return <GlobalContext.Provider>
-        {Children}
+export default function GlobalState({ children }) {
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
+      const data = await res.json();
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false)
+        setSearchParam('')
+      }
+      console.log(recipeList);
+    } catch (error) {
+      console.log(error);
+      setLoading(false)
+      setSearchParam('')
+
+    }
+  }
+  return (
+    <GlobalContext.Provider
+      value={{ searchParam, setSearchParam, handleSubmit }}
+    >
+      {children}
     </GlobalContext.Provider>
+  );
 }
